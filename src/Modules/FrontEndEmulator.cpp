9de5fe2185534c57cba69a2942f1fdc5b367ca67
aagaard <Brian.Petersen@cern.ch>
2019-05-12 16:42:08 +0200

    Add option for various corruptions

--- src/Modules/FrontEndEmulator.cpp
+++ src/Modules/FrontEndEmulator.cpp
@@ -14,10 +14,15 @@ FrontEndEmulator::FrontEndEmulator() {
 
   m_meanSize = cfg["meanSize"];
   m_rmsSize = cfg["rmsSize"];
-  m_fragID = cfg["fragmentID"];  // to be made configurable
-  INFO("Generating fragment with mean of "<<m_meanSize
+  m_fragID = cfg["fragmentID"];  
+  INFO("Generating fragment "<<m_fragID<<" with mean of "<<m_meanSize
        <<" bytes and RMS of "<<m_rmsSize<<" bytes");
-
+  
+  m_probMissTrig = cfg["probMissingTrigger"];
+  m_probMissFrag = cfg["probMissingFragment"];
+  m_probCorruptFrag = cfg["probCorruptedFragment"];
+		      
+  
   if (m_trigIn.init(cfg["triggerPort"])) {
     ERROR("Cannot bind trigger port");
     exit(1);
@@ -49,6 +54,7 @@ void FrontEndEmulator::runner() {
   INFO(" Running...");
   std::default_random_engine generator;
   std::normal_distribution<float> gaussian(m_meanSize,m_rmsSize);
+  std::uniform_real_distribution<> flat(0.0, 1.0);
   RawFragment data;
   for(int ii=0;ii<MAXFRAGSIZE;ii++) data.data[ii]=m_fragID;
   while (m_run) {
@@ -59,6 +65,10 @@ void FrontEndEmulator::runner() {
       WARNING("Received "<<num<<" bytes");
       continue;
     }
+    if (flat(generator)<m_probMissTrig) {
+      INFO("Emulating missed trigger, global id:"<<buffer.event_id<<" - local counter: "<<m_eventCounter);
+      continue;
+    }
     m_eventCounter++;
     INFO("Event "<<m_eventCounter<<": "<<buffer.event_id<<" BC:"<<buffer.bc_id);
     data.source_id=m_fragID;
@@ -66,7 +76,19 @@ void FrontEndEmulator::runner() {
     data.bc_id=buffer.bc_id;
     data.dataLength=std::min(std::max(int(gaussian(generator)),0),MAXFRAGSIZE);
     INFO("Fragment size: "<<data.size());
-    m_outHandle.send(&data,data.size());
+    if (flat(generator)<m_probMissFrag) {
+      INFO("Emulating missed fragment for global id:"<<buffer.event_id);
+      continue;
+    }
+    int numBytes=data.size();
+    if (flat(generator)<m_probCorruptFrag) {
+      data.event_id=uint32_t(flat(generator)*0xFFFFFFFF);
+      data.source_id=uint32_t(flat(generator)*0xFFFFFFFF);
+      data.bc_id=uint16_t(flat(generator)*0xFFFF);
+      data.dataLength=uint16_t(flat(generator)*0xFFFF);
+      numBytes=std::min(int(flat(generator)*0xFFFF),MAXFRAGSIZE);
+    }
+    m_outHandle.send(&data,numBytes);
   }
   INFO(" Runner stopped");
 }

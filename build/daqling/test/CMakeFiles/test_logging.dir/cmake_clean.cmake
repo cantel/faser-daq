cdc655c224631ed45f274f676694eacbde56ab67
Enrico Gamberini <enrico.gamberini@cern.ch>
2019-05-15 11:39:37 +0200

    Revert "Revert "Marco help me""
    This reverts commit 99cac1453898e6fe27671f695b3d52a0bcfcce7e.

---
+++ build/daqling/test/CMakeFiles/test_logging.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_logging.dir/src/test_logging.cpp.o"
+  "../../bin/test_logging.pdb"
+  "../../bin/test_logging"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_logging.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/engamber/workspace/daqling_top/src/Test.cpp" "/home/engamber/workspace/daqling_top/build/src/CMakeFiles/test.dir/Test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/../include"
+  "../src/../daqling/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/engamber/workspace/daqling_top/build/daqling/src/Core/CMakeFiles/core.dir/DependInfo.cmake"
+  "/home/engamber/workspace/daqling_top/build/daqling/src/Utilities/CMakeFiles/utilities.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

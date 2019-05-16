bf6463f0978e58bb810a47f6b28d8cf359ec334f
cantel <claire.antel@cern.ch>
2019-05-16 10:12:16 +0200

    implementing first test hist using boost. includes hot fixes to run boost 1.7.

--- src/Modules/MonitorBinary.cpp
+++ src/Modules/MonitorBinary.cpp
@@ -3,6 +3,13 @@
 /// \endcond
 using namespace std::chrono_literals;
 
+
+#include <boost/format.hpp>
+#include <boost/histogram.hpp>
+using namespace boost::histogram;
+#include <iostream> // std::flush
+#include <sstream> // std::ostringstream
+
 struct header_t {
   uint16_t payload_size;
   uint16_t source_id;
@@ -27,13 +34,13 @@ extern "C" void destroy_object(Monitor *object) { delete object; }
 Monitor::Monitor() { INFO("Monitor::Monitor"); }
 
 Monitor::~Monitor() { 
-  //INFO(__METHOD_NAME__ << " With config: " << m_config.dump() << " getState: " << this->getState());
-  INFO(__METHOD_NAME__ << " getState: " << this->getState());
+  INFO(__METHOD_NAME__ << " With config: " << m_config.dump() << " getState: " << this->getState());
  }
 
 void Monitor::start() {
   DAQProcess::start();
   INFO(__METHOD_NAME__ << " getState: " << this->getState());
+
 }
 
 void Monitor::stop() {
@@ -43,15 +50,36 @@ void Monitor::stop() {
 
 void Monitor::runner() {
   INFO(__METHOD_NAME__ << " Running...");
+
+  auto testhist = make_histogram(axis::regular<>(150, -0.5, 149.5, "x"));
+ 
   while (m_run) {
+    INFO(__METHOD_NAME__ << "just checked. still running ..");
     daq::utilities::Binary b1;
     while(!m_connections.get(1, b1) && m_run) {
       std::this_thread::sleep_for(100ms);
     }
+    //while ( m_connections.get(1, b1) && m_run ) {
     INFO("b1.size() =" << b1.size());
-    const data_t * unpackeddata((data_t *)malloc(b1.size()));
-    unpackeddata = static_cast<const data_t *>(b1.data());
-    INFO(__METHOD_NAME__ <<  " data sequence number "<< unpackeddata->header.seq_number);
+    if ( b1.size() > 0 ) {
+        const data_t * unpackeddata((data_t *)malloc(b1.size()));
+    	unpackeddata = static_cast<const data_t *>(b1.data());
+    	INFO(__METHOD_NAME__ <<  " data sequence number "<< unpackeddata->header.seq_number);
+    	INFO(__METHOD_NAME__ <<  " data payload size "<< unpackeddata->header.payload_size);
+    	testhist(unpackeddata->header.payload_size);
+    }
   }
+
+  INFO(__METHOD_NAME__ << " flushing testhist info ... ");
+
+  // stolen from c++ site.
+  std::ostringstream os;
+  for (auto x : indexed(testhist, coverage::all)) {
+    os << boost::format("bin %2i [%4.1f, %4.1f): %i\n") % x.index() % x.bin().lower() %
+              x.bin().upper() % *x;
+  }
+
+  std::cout << os.str() << std::flush;
+
   INFO(__METHOD_NAME__ << " Runner stopped");
 }

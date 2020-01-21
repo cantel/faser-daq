16daeec2e754474a4533ba9def18da93a97b67d8
Ondrej Theiner <otheiner@epdtdi-daq01.cern.ch>
2020-01-21 13:03:27 +0100

    The first version that has probably all necessary code - some code needs to be uncommented and tested, though.

--- src/Modules/TrackerReceiver/TrackerReceiverModule.cpp
+++ src/Modules/TrackerReceiver/TrackerReceiverModule.cpp
@@ -18,6 +18,9 @@
 #include "TrackerReceiverModule.hpp"
 #include "TrackerReadout/TRBAccess.h"
 #include "GPIOBase/DummyInterface.h"
+#include "Commons/FaserProcess.hpp"
+#include "Commons/EventFormat.hpp"
+#include "Commons/RawExampleFormat.hpp"
 #include <string>
 #include <iostream>
 
@@ -88,7 +91,12 @@ void TrackerReceiverModule::stop() {
 void TrackerReceiverModule::runner() {
   INFO("Running...");
   std::vector<std::vector<uint32_t>> vector_of_raw_events;
-  
+  uint32_t* raw_payload[MAXFRAGSIZE];
+  uint8_t  local_fragment_tag = EventTags::PhysicsTag;
+  uint32_t local_source_id    = SourceIDs::TrackerSourceID;
+  uint64_t local_event_id;
+  uint16_t local_bc_id;
+
   while (m_run) {
     vector_of_raw_events = m_trb->GetTRBEventData();
 
@@ -96,12 +104,25 @@ void TrackerReceiverModule::runner() {
     for(auto event : vector_of_raw_events){
       counter += 1;
       std::cout << "printing event " << counter << std::endl;
+
+      *raw_payload = event.data();
+      /*//TODO Decode following two numbers from raw data by event decoder
+      local_event_id = 
+      local_bc_id = 
+      std::unique_ptr<EventFragment> fragment(new EventFragment(local_fragment_tag, local_source_id, 
+                                              local_event_id, local_bc_id, Binary(raw_payload, total_size)));
+      // TODO : What is the status supposed to be?
+        uint16_t status=0;
+          fragment->set_status( status );
+      
+            // place the raw binary event fragment on the output port
+              m_connections.put(0, const_cast<Binary&>(fragment->raw()));*/
+
+      //Following for-cycle is only for debugging
       for(auto word : event){
         std::cout << word << std::endl;
       }
-    }
-  
-  //m_trb->PollData();   
+    }   
   }
   INFO("Runner stopped");
 }

4a69529ef1f0cf572802c56f920c7446329bc67f
Claire Antel <claire.antel@cern.ch>
2020-03-03 13:04:16 +0100

    more TLBMonitor histograms and adding getters to TLBMonitoringFragment data format

--- src/Commons/RawExampleFormat.hpp
+++ src/Commons/RawExampleFormat.hpp
@@ -38,31 +38,61 @@ struct MonitoringFragment {  // will eventually be detector specific fragment...
 }  __attribute__((__packed__));
 
 struct TLBMonitoringFragment { 
-  uint32_t header;
-  uint32_t event_id;
-  uint32_t orbit_id;
-  uint32_t bc_id;
-  uint32_t tbp0;
-  uint32_t tbp1;
-  uint32_t tbp2;
-  uint32_t tbp3;
-  uint32_t tbp4;
-  uint32_t tbp5;
-  uint32_t tap0;
-  uint32_t tap1;
-  uint32_t tap2;
-  uint32_t tap3;
-  uint32_t tap4;
-  uint32_t tap5;
-  uint32_t tav0;
-  uint32_t tav1;
-  uint32_t tav2;
-  uint32_t tav3;
-  uint32_t tav4;
-  uint32_t tav5;
-  uint32_t deadtime_veto_counter;
-  uint32_t busy_veto_counter;
-  uint32_t rate_limiter_veto_counter;
-  uint32_t bcr_veto_counter;
+  public:
+    // getters
+    uint32_t header() { return m_header; }
+    uint32_t event_id() { return m_event_id; }
+    uint32_t orbit_id() { return m_orbit_id; }
+    uint32_t bc_id() { return m_bc_id; }
+    uint32_t tbp0() { return m_tbp0; }
+    uint32_t tbp1() { return m_tbp1; }
+    uint32_t tbp2() { return m_tbp2; }
+    uint32_t tbp3() { return m_tbp3; }
+    uint32_t tbp4() { return m_tbp4; }
+    uint32_t tbp5() { return m_tbp5; }
+    uint32_t tap0() { return m_tap0; }
+    uint32_t tap1() { return m_tap1; }
+    uint32_t tap2() { return m_tap2; }
+    uint32_t tap3() { return m_tap3; }
+    uint32_t tap4() { return m_tap4; }
+    uint32_t tap5() { return m_tap5; }
+    uint32_t tav0() { return m_tav0; }
+    uint32_t tav1() { return m_tav1; }
+    uint32_t tav2() { return m_tav2; }
+    uint32_t tav3() { return m_tav3; }
+    uint32_t tav4() { return m_tav4; }
+    uint32_t tav5() { return m_tav5; }
+    uint32_t deadtime_veto_counter() { return m_deadtime_veto_counter; }
+    uint32_t busy_veto_counter() { return m_busy_veto_counter; }
+    uint32_t rate_limiter_veto_counter() { return m_rate_limiter_veto_counter; }
+    uint32_t bcr_veto_counter() { return m_bcr_veto_counter; }
+
+  private:
+    uint32_t m_header;
+    uint32_t m_event_id;
+    uint32_t m_orbit_id;
+    uint32_t m_bc_id;
+    uint32_t m_tbp0;
+    uint32_t m_tbp1;
+    uint32_t m_tbp2;
+    uint32_t m_tbp3;
+    uint32_t m_tbp4;
+    uint32_t m_tbp5;
+    uint32_t m_tap0;
+    uint32_t m_tap1;
+    uint32_t m_tap2;
+    uint32_t m_tap3;
+    uint32_t m_tap4;
+    uint32_t m_tap5;
+    uint32_t m_tav0;
+    uint32_t m_tav1;
+    uint32_t m_tav2;
+    uint32_t m_tav3;
+    uint32_t m_tav4;
+    uint32_t m_tav5;
+    uint32_t m_deadtime_veto_counter;
+    uint32_t m_busy_veto_counter;
+    uint32_t m_rate_limiter_veto_counter;
+    uint32_t m_bcr_veto_counter;
 }  __attribute__((__packed__));
 
--- src/Modules/TLBMonitor/TLBMonitorModule.cpp
+++ src/Modules/TLBMonitor/TLBMonitorModule.cpp
@@ -46,15 +46,22 @@ void TLBMonitorModule::monitor(daqling::utilities::Binary &eventBuilderBinary) {
   uint16_t payloadSize = m_fragment->payload_size(); 
 
   m_histogrammanager->fill("h_tlb_payloadsize", payloadSize);
+  m_histogrammanager->fill("h_tlb_BCID", m_tlbmonitoringFragment->bc_id);
   m_metric_payload = payloadSize;
 
-  m_metric_event_id = m_tlbmonitoringFragment->event_id;
+  m_metric_event_id = m_tlbmonitoringFragment->event_id();
 
  // veto counters
-  m_deadtime_veto_counter = m_tlbmonitoringFragment->deadtime_veto_counter;
-  m_busy_veto_counter = m_tlbmonitoringFragment->busy_veto_counter;
-  m_rate_limiter_veto_counter = m_tlbmonitoringFragment->rate_limiter_veto_counter;
-  m_bcr_veto_counter = m_tlbmonitoringFragment->bcr_veto_counter;
+  m_deadtime_veto_counter += m_tlbmonitoringFragment->deadtime_veto_counter();
+  m_busy_veto_counter += m_tlbmonitoringFragment->busy_veto_counter();
+  m_rate_limiter_veto_counter += m_tlbmonitoringFragment->rate_limiter_veto_counter();
+  m_bcr_veto_counter += m_tlbmonitoringFragment->bcr_veto_counter();
+  m_histogrammanager->fill("h_tlb_bcrcount_vs_eventid", m_tlbmonitoringFragment->event_id(), float(m_tlbmonitoringFragment->bcr_veto_counter()));
+  m_histogrammanager->fill("h_tlb_ratelimiter_vs_eventid", m_tlbmonitoringFragment->event_id(), float(m_tlbmonitoringFragment->rate_limiter_veto_counter()));
+  m_histogrammanager->fill("h_tlb_veto_counts", "SimpleDeadtime", float(m_deadtime_veto_counter));
+  m_histogrammanager->fill("h_tlb_veto_counts", "Busy", float(m_busy_veto_counter));
+  m_histogrammanager->fill("h_tlb_veto_counts", "RateLimiter", float(m_rate_limiter_veto_counter));
+  m_histogrammanager->fill("h_tlb_veto_counts", "BCR", float(m_bcr_veto_counter));
 
   // 2D hist fill
   //m_histogrammanager->fill("h_tlb_numfrag_vs_sizefrag", m_monitoringFragment->num_fragments_sent/1000., m_monitoringFragment->size_fragments_sent/1000.);
@@ -65,8 +72,15 @@ void TLBMonitorModule::register_hists() {
   INFO(" ... registering histograms in TLBMonitor ... " );
   
   m_histogrammanager->registerHistogram("h_tlb_payloadsize", "payload size [bytes]", -0.5, 545.5, 275, 3.);
-  std::vector<std::string> categories = {"Ok", "Unclassified", "BCIDMistmatch", "TagMismatch", "Timeout", "Overflow","Corrupted", "Dummy", "Missing", "Empty", "Duplicate", "DataUnpack"};
-  m_histogrammanager->registerHistogram("h_tlb_errorcount", "error type", categories, 5. );
+  m_histogrammanager->registerHistogram("h_tlb_BCID", "BCID", -0.5, 3564.5, 3565, 5.);
+  m_histogrammanager->registerHistogram("h_tlb_bcrcount_vs_eventid", "eventID","BCR veto cnt", 0, 5000, 5000, Axis::Range::EXTENDABLE, 5.);
+  m_histogrammanager->registerHistogram("h_tlb_ratelimiter_vs_eventid", "eventID", "RateLimiter veto cnt", 0, 5000, 5000, Axis::Range::EXTENDABLE, 5.);
+  std::vector<std::string> error_categories = {"Ok", "Unclassified", "BCIDMistmatch", "TagMismatch", "Timeout", "Overflow","Corrupted", "Dummy", "Missing", "Empty", "Duplicate", "DataUnpack"};
+
+  m_histogrammanager->registerHistogram("h_tlb_errorcount", "error type", error_categories, 5. );
+
+  std::vector<std::string> veto_categories = {"SimpleDeadtime", "Busy", "RateLimiter", "BCR"};;
+  m_histogrammanager->registerHistogram("h_tlb_veto_counts", "veto type", veto_categories, 5. );
   // example 2D hist
   //m_histogrammanager->register2DHistogram("h_tlb_numfrag_vs_sizefrag", "no. of sent fragments", -0.5, 30.5, 31, "size of sent fragments [kB]", -0.5, 9.5, 20, 5 );
 
@@ -82,19 +96,19 @@ void TLBMonitorModule::register_metrics() {
   register_error_metrics();
 
   m_metric_payload = 0;
-  m_statistics->registerMetric(&m_metric_payload, "payload", daqling::core::metrics::LAST_VALUE);
+  m_statistics->registerMetric(&m_metric_payload, "payload", daqling::core::metrics::LAST);
 
   m_metric_event_id = 0;
-  m_statistics->registerMetric(&m_metric_event_id, "eventID", daqling::core::metrics::LAST_VALUE);
+  m_statistics->registerMetric(&m_metric_event_id, "eventID", daqling::core::metrics::LAST);
   //veto counters
   m_deadtime_veto_counter = 0;
-  m_statistics->registerMetric(&m_deadtime_veto_counter, "deadtimeVetoCounter", daqling::core::metrics::LAST_VALUE);
+  m_statistics->registerMetric(&m_deadtime_veto_counter, "deadtimeVetoCounter", daqling::core::metrics::ACCUMULATE);
   m_busy_veto_counter = 0;
-  m_statistics->registerMetric(&m_busy_veto_counter, "busyVetoCounter", daqling::core::metrics::LAST_VALUE);
+  m_statistics->registerMetric(&m_busy_veto_counter, "busyVetoCounter", daqling::core::metrics::ACCUMULATE);
   m_rate_limiter_veto_counter = 0;
-  m_statistics->registerMetric(&m_rate_limiter_veto_counter, "ratelimiterVetoCounter", daqling::core::metrics::LAST_VALUE);
+  m_statistics->registerMetric(&m_rate_limiter_veto_counter, "ratelimiterVetoCounter", daqling::core::metrics::ACCUMULATE);
   m_bcr_veto_counter = 0;
-  m_statistics->registerMetric(&m_bcr_veto_counter, "BCRVetoCounter", daqling::core::metrics::LAST_VALUE);
+  m_statistics->registerMetric(&m_bcr_veto_counter, "BCRVetoCounter", daqling::core::metrics::ACCUMULATE);
 
 
   return;

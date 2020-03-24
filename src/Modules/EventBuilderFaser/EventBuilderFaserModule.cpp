0087be6be92b66ebffed4167c43063fe1ecc588a
aagaard <Brian.Petersen@cern.ch>
2020-03-24 19:22:35 +0100

    Added streaming to event builder

--- src/Modules/EventBuilderFaser/EventBuilderFaserModule.cpp
+++ src/Modules/EventBuilderFaser/EventBuilderFaserModule.cpp
@@ -17,10 +17,9 @@ EventBuilderFaserModule::EventBuilderFaserModule() {
   auto cfg = m_config.getSettings();
 
   m_maxPending = cfg.value("maxPending",10);
-  m_timeout = cfg.value("timeout_ms",1000);
+  m_timeout = 1000*cfg.value("timeout_ms",1000);
 
   m_numChannels=m_config.getConnections()["receivers"].size();
-  m_numOutChannels=m_config.getConnections()["senders"].size();
 
 }
 
@@ -29,162 +28,157 @@ EventBuilderFaserModule::~EventBuilderFaserModule() { }
 
 void EventBuilderFaserModule::configure() {
   FaserProcess::configure();
-  registerVariable(m_physicsEventCount, "PhysicsEvents");
-  registerVariable(m_physicsEventCount, "PhysicsRate", metrics::RATE);
-  registerVariable(m_monitoringEventCount, "MonitoringEvents");
-  registerVariable(m_monitoringEventCount, "MonitoringRate", metrics::RATE);
-  registerVariable(m_calibrationEventCount, "CalibrationEvents");
-  registerVariable(m_calibrationEventCount, "CalibrationRate", metrics::RATE);
+  registerVariable(m_eventCounts[EventTags::PhysicsTag], "PhysicsEvents");
+  registerVariable(m_eventCounts[EventTags::PhysicsTag], "PhysicsRate", metrics::RATE);
+  registerVariable(m_eventCounts[EventTags::MonitoringTag], "MonitoringEvents");
+  registerVariable(m_eventCounts[EventTags::MonitoringTag], "MonitoringRate", metrics::RATE);
+  registerVariable(m_eventCounts[EventTags::TLBMonitoringTag], "TLBMonitoringEvents");
+  registerVariable(m_eventCounts[EventTags::TLBMonitoringTag], "TLBMonitoringRate", metrics::RATE);
+  registerVariable(m_eventCounts[EventTags::CalibrationTag], "CalibrationEvents");
+  registerVariable(m_eventCounts[EventTags::CalibrationTag], "CalibrationRate", metrics::RATE);
   registerVariable(m_run_number, "RunNumber");
   registerVariable(m_run_start, "RunStart");
   registerVariable(m_corruptFragmentCount, "CorruptFragmentErrors");
-  registerVariable(m_duplicateCount, "DuplicateEventErrors");
   registerVariable(m_duplicateSourceCount, "DuplicateSourceErrors");
-  registerVariable(m_overflowCount, "OverflowEventErrors");
   registerVariable(m_timeoutCount, "TimeoutEventErrors");
   registerVariable(m_BCIDMismatchCount, "BCIDMisMatches");
-  registerVariable(m_queueFraction, "QueueFraction");
 }
 
 void EventBuilderFaserModule::start(unsigned int run_num) {
   FaserProcess::start(run_num);
   m_run_number = run_num; 
   m_run_start = std::time(nullptr);
+  for(int ii=0;ii<MaxAnyTag;ii++) m_eventCounts[ii]=0;
+  m_corruptFragmentCount=0;
+  m_duplicateSourceCount=0;
+  m_timeoutCount=0;
+  m_BCIDMismatchCount=0;
   m_status = STATUS_OK;
   INFO("getState: " << getState());
 }
 
 void EventBuilderFaserModule::stop() {
+  std::this_thread::sleep_for(std::chrono::microseconds(2*m_timeout)); //wait for events to for sure be timed out
   FaserProcess::stop();
   INFO("getState: " << this->getState());
 }
 
 
-bool EventBuilderFaserModule::sendEvent(uint8_t event_tag,
-					uint64_t event_number,
-					int outChannel,
-					std::vector<const EventFragment *>& fragments) {
-      EventFull event(event_tag,m_run_number,event_number);
+bool EventBuilderFaserModule::sendEvent(uint8_t event_tag,EventFull *event) {
+  int channel=event_tag+10; //FIXME should use different offset
+  INFO("Sending event "<<event->event_id()<<" - "<<event->size()<<" bytes on channel "<<channel);
+  auto *bytestream=event->raw();
+  daqling::utilities::Binary binData(bytestream->data(),bytestream->size());
+  m_connections.put(channel,binData);
+  delete bytestream;
+  return true;
+}
 
-      for(unsigned int ch=0;ch<fragments.size();ch++) {
-	if (fragments.at(ch)) {
-	  try {
-	    auto status=event.addFragment(fragments[ch]);
-	    if (status&EventStatus::BCIDMismatch) {
-	      WARNING("Mismatch in BCID for event "<<event_number<<" : "<<event.bc_id()<<" != "<<fragments[ch]->bc_id());
-	      m_BCIDMismatchCount++;
-	    }
-	  } catch (const std::runtime_error& e) {
-	    ERROR("Got error in fragment: "<<e.what());
-	    m_duplicateSourceCount++;
-	  }
-	} else {
-	  WARNING("Missing fragment for event "<<event_number<<": No fragment for channel "<<ch);
-	  event.updateStatus(EventStatus::MissingFragment);
-	}
-      }
-      INFO("Sending event "<<event.event_id()<<" - "<<event.size()<<" bytes on channel "<<outChannel);
-      auto *bytestream=event.raw();
-      daqling::utilities::Binary binData(bytestream->data(),bytestream->size());
-      m_connections.put(outChannel,binData);
-      delete bytestream;
-      return true;
+void EventBuilderFaserModule::addFragment(EventFragment *fragment) {
+  auto event_id=fragment->event_id();
+  auto fragment_tag=fragment->fragment_tag();
+  auto status=fragment->status();
+  uint8_t event_tag=fragment_tag;
+  
+  if ((fragment_tag!=EventTags::CorruptedTag) &&
+      (fragment_tag!=EventTags::DuplicateTag) &&
+      (fragment_tag>EventTags::MaxRegularTag)) {
+    event_tag=EventTags::CorruptedTag; //reroute unknown tags
+  }
+  
+  if (status & EventStatus::CorruptedFragment) {
+    m_corruptFragmentCount++;
+    event_tag=EventTags::CorruptedTag; //reroute
+  }
+
+  auto& pendingEvents=m_pendingEvents[event_tag];
+  if (pendingEvents.find(event_id)==pendingEvents.end()) {
+    pendingEvents[event_id]=new EventFull(event_tag,m_run_number,++m_eventCounts[event_tag]);
+  }
+
+  auto event=pendingEvents[event_id];
+  if (!event) THROW(EventBuilderException,"Out of memory");
+  
+  try {
+    auto status=event->addFragment(fragment);
+    if (status&EventStatus::BCIDMismatch) {
+      WARNING("Mismatch in BCID for event "<<event->event_id()<<" : "<<event->bc_id()<<" != "<<fragment->bc_id());
+      m_BCIDMismatchCount++;
+    }
+  }
+  catch (const std::runtime_error& e) {
+    ERROR("Got error in fragment: "<<e.what());
+    m_duplicateSourceCount++;
+    if (event_tag!=EventTags::DuplicateTag) { //reroute to duplicate stream unless already tried that
+      fragment->set_fragment_tag(EventTags::DuplicateTag);
+      addFragment(fragment);
+    } else {
+      ERROR("Failed to transmit duplicate fragment");
+      delete fragment; // just give up
+    }
+    return;
+  }
+  // for now hardcoded that only physics events have multiple fragments
+  // anything else gets sent immediately
+  if (((event_tag==EventTags::PhysicsTag) && (event->fragment_count()==m_numChannels)) ||
+      (event_tag!=EventTags::PhysicsTag)) { 
+    m_readyEvents[event_tag].insert(event_id);
+  }
 }
 
 
-struct pendingEvent_t {
-  time_point<steady_clock> start_time;
-  unsigned int fragmentCount;
-  std::vector<const EventFragment *> fragments;
-};
 
 void EventBuilderFaserModule::runner() {
   INFO("Running...");
 
-  unsigned int channelNum=0;
   bool noData=true;
-  std::map<uint64_t, pendingEvent_t> pendingEvents;
   daqling::utilities::Binary  blob;
-  while (m_run) { //BP: At the moment there is no flushing of partial events on stop
-    m_queueFraction=1.0*pendingEvents.size()/m_maxPending;
-    if (m_queueFraction>0.5) { //BP: not a good example of status flag filling as nothing else is considered
-      m_status=STATUS_WARN;
-      if (m_queueFraction>0.85) {
-	m_status=STATUS_ERROR;
-      }
-    } else {
-      m_status=STATUS_OK;
-    }
-    
-    int channel=channelNum;
-    channelNum++;
-    if (channelNum>=m_numChannels) channelNum=0;
-    
-    if (!m_connections.get(channel, blob)) {
-      if (noData && channelNum==0) std::this_thread::sleep_for(10ms);
-      noData=true;
-      continue;
-    }
-    noData=false;
-    const EventFragment* fragment;
-    try {
-      fragment = new EventFragment(blob.data<uint8_t *>(),blob.size());
-    } catch (const std::runtime_error& e) {
-      ERROR("Got error in fragment ("<<blob.size()<<" bytes) from channel "<<channel<<": "<<e.what());
-      INFO("Received:\n"<<blob);
-      m_corruptFragmentCount++;
-      continue;
-    }
-    auto event_id=fragment->event_id();
-    auto fragment_tag=fragment->fragment_tag(); 
-    if ((fragment_tag==MonitoringTag) || (fragment_tag==TLBMonitoringTag)) {
-      INFO("Got monitoring fragment : "<<event_id<<" from channel "<<channel);
-      //send monitoring fragments immediately as no other fragments are expected for thos
-      std::vector<const EventFragment *> monFragmentList;
-      monFragmentList.push_back(fragment);
-      sendEvent(fragment_tag,m_monitoringEventCount,m_numChannels,monFragmentList);
-      m_monitoringEventCount++;
-      continue;
-    }
- 
-    INFO("Got data fragment : "<<event_id<<" from channel "<<channel << " with size " << blob.size());
-    if (pendingEvents.find(event_id)==pendingEvents.end()) { //got first fragment in a new event
-      struct pendingEvent_t pending;
-      pending.start_time=steady_clock::now();
-      pending.fragmentCount=0;
-      pending.fragments=std::vector<const EventFragment *>(m_numChannels,0);
-      pendingEvents[event_id]=pending;
-    }
-    int ch=channel;
-    if (pendingEvents[event_id].fragments[ch]!=0) { 
-      //BP: this won't catch if an incomplete event was already sent
-      ERROR("Got same event from same source more than once!");
-      m_duplicateCount++;
-    } else {
-      pendingEvents[event_id].fragmentCount++;
-    }
-    pendingEvents[event_id].fragments[ch]=fragment;
+  while (m_run) { 
+    if (m_timeoutCount>10) m_status=STATUS_WARN;
+    if (m_timeoutCount>100) m_status=STATUS_ERROR;
 
-    uint64_t eventToSend=0;
-    if (pendingEvents[event_id].fragmentCount==m_numChannels) {
-      eventToSend=event_id;
-    } else if (pendingEvents.size()>m_maxPending) {
-      eventToSend=pendingEvents.begin()->first;
-      WARNING("Too many events pending - will send first incomplete event (EventID: "<<eventToSend<<")");
-      m_overflowCount++;
-    } else {
-      auto oldestEvent=pendingEvents.begin()->second;
-      if ( duration_cast<std::chrono::milliseconds>(steady_clock::now()-oldestEvent.start_time).count()>m_timeout) {
-	eventToSend=pendingEvents.begin()->first;
-	WARNING("Incomplete event timed out (EventID: "<<eventToSend<<")");
-	m_timeoutCount++;
+    noData=true;
+    for(unsigned int channel=0;channel<m_numChannels;channel++) {
+      if (m_connections.get(channel, blob)) {
+	noData=false;
+	EventFragment* fragment;
+	try {
+	  fragment = new EventFragment(blob.data<uint8_t *>(),blob.size());
+	} catch (const std::runtime_error& e) {
+	  ERROR("Got error in fragment ("<<blob.size()<<" bytes) from channel "<<channel<<": "<<e.what());
+	  m_corruptFragmentCount++;
+	  fragment = new EventFragment(EventTags::CorruptedTag,channel,
+				       0xFFFFFFFF,0xFFFF,
+				       blob.data<void *>(),blob.size());
+	  fragment->set_status(EventStatus::ErrorFragment);
+	}
+	addFragment(fragment);
       }
     }
-    
-    if (eventToSend) {
-      sendEvent(PhysicsTag,m_physicsEventCount,m_numChannels,pendingEvents[eventToSend].fragments);
-      pendingEvents.erase(pendingEvents.find(eventToSend));
-      m_physicsEventCount++;
+    if (noData) std::this_thread::sleep_for(10ms);
+
+    // send any events ready or timed out
+    microseconds now;
+    now = duration_cast<microseconds>(system_clock::now().time_since_epoch());
+    for(unsigned int tag=0;tag<MaxAnyTag;tag++) {
+      for( auto & event_id : m_readyEvents[tag]) {
+	sendEvent(tag,m_pendingEvents[tag][event_id]);
+	delete m_pendingEvents[tag][event_id];
+	m_pendingEvents[tag].erase(event_id);
+      }
+      m_readyEvents[tag].clear();
+      // check oldest event
+      if (m_pendingEvents[tag].size()) {
+	auto event=m_pendingEvents[tag].begin()->second;
+	if ((now.count()-event->timestamp())>m_timeout) {
+	  event->updateStatus(EventStatus::MissingFragment);
+	  WARNING("Missing fragments for "<<event->event_id());
+	  m_timeoutCount++;
+	  sendEvent(tag,event);
+	  delete event;
+	  m_pendingEvents[tag].erase(m_pendingEvents[tag].begin());
+	}
+      }
     }
   }
   INFO("Runner stopped");

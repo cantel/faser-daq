1f1e05f52a88256877a670b937ae9672f41a9838
cantel <claire.antel@cern.ch>
2020-06-17 13:03:49 +0200

    Revert "implementing basic histogram web display. very much still work in progress..."
    This reverts commit 13003275e7b4933ad0b64c95eb5d5292d0c8296f.

--- scripts/Web/static/js/histograms.js
+++
@@ -1,8 +0,0 @@
-function updateHistogram(id, histName) {
-    // Create the charts
-    const source = new EventSource("/histogramming/"+id+"/"+histName+"/lastValues/5.0");
-    source.onmessage = function (event) {
-	const histdata = JSON.parse(event.data);
-        Plotly.react(id, histdata);
-    };
-}

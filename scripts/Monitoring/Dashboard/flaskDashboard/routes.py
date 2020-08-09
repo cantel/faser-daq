8448491ffa0fa0e1e064cd1899a1ba553dbc31a5
karlmtr <none>
2020-08-09 18:46:19 +0200

    updating the graphs doesn't reset the view anymore

--- scripts/Monitoring/Dashboard/flaskDashboard/routes.py
+++ scripts/Monitoring/Dashboard/flaskDashboard/routes.py
@@ -39,17 +39,22 @@ def getcurrentModulesFUNCTION():
 plotly_types = {'num_fixedwidth': "bar", '2d_num_fixedwidth': "histogram2d"}
 r = redis.Redis(host='localhost', port=6379, db=0,charset="utf-8", decode_responses=True)
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-  if not request.form.get('module_select'):
+  # if not request.form.get('module_select'):
+  if request.method == 'POST':
+    session["flash_module"] = True
+    selectedModule = request.form.get('module_select')
+    return redirect(url_for('monitormodule', module = selectedModule))
+  else:
+    # if session.get("flash_module")== True:
+    flash("Modules updated", "success")
+    session["flash_module"]= False
     print("Doing the busy stuff")
     dataModules = getcurrentModulesFUNCTION()
     session['modules'] = dataModules
     return render_template('home.html', modules = dataModules["data"])
-
-  elif request.method == 'POST':
-    selectedModule = request.form.get('module_select')
-    return redirect(url_for('monitormodule', module = selectedModule))
 
 
 @app.route('/monitor/<string:module>', methods=["GET", "POST"])
@@ -80,11 +85,14 @@ def lastHistogram(sourceid, nameshisto):
           yaxis= dict(title = dict(text=histobj["ylabel"]))
 
           layout = dict( 
-            title = dict(text=histobj['name']),
-            width = 1024,
-            height = 600,
+            # title = dict(text=histobj['name']),
+            autosize = False,
+            uirevision = True,
+            # width = 1024,
+            # height = 600,
             xaxis = xaxis,
-            yaxis = yaxis
+            yaxis = yaxis,
+            margin=dict(l=50,r=50,b=50,t=50,pad=4)
           )
           data =[]
           hist_type = histobj["type"]
@@ -99,28 +107,52 @@ def lastHistogram(sourceid, nameshisto):
             xmax = float(histobj["xmax"])
             xbins = int(histobj["xbins"])
             xstep = (xmax-xmin)/float(xbins)
+            xarray = [ xmin+xstep*xbin for xbin in range(xbins) ]
             ymin = float(histobj["ymin"])
             ymax = float(histobj["ymax"])
             ybins = int(histobj["ybins"])
             ystep = (ymax-ymin)/float(ybins)
-            data = [dict(xbins=dict(start=xmin, end=xmax,size=xstep), ybins=dict(start=ymin, end=ymax,size=ystep), z=zarray[106:], type="histogram2d")]
+            yarray = [ ymin+ystep*ybin for ybin in range(ybins) ]
+            print("xbins:", xbins)
+            print("xstep:", xstep)
+            print("ybins:", ybins)
+            print("ystep:", ystep)
+            print("xarray", len(xarray))
+            print("yarray", len(xarray))
+            print("\n \n \n \n \n ")
+            
+            # data = [dict(xbins=dict(start=xmin, end=xmax,size=xstep), ybins=dict(start=ymin, end=ymax,size=ystep), z=zarray[106:], type="heatmap")]
+            # data = [dict(x0 = xmin, dx = xstep, y0 = ymin, dy = ystep, z = zarray, type="heatmap")]
+            data = [dict(x = xarray, y = yarray, z = zarray, type="heatmap")]
           #  print("xbins = ", xbins)
           #  print("ybins = ", ybins)
           #  print("len(zvalues) = ", len(zarray))
           else:
-              xmin = float(histobj["xmin"])
-              xmax = float(histobj["xmax"])
-              xbins = int(histobj["xbins"])
-              step = (xmax-xmin)/float(xbins)
-              xarray = [ xmin+step*xbin for xbin in range(xbins) ]
-              yarray = histobj["yvalues"]
-              data = [dict(x0=xmin, dx=step, y=yarray, type="bar")]
-
-          fig = dict(data=data, layout=layout)
+            xmin = float(histobj["xmin"])
+            xmax = float(histobj["xmax"])
+            xbins = int(histobj["xbins"])
+            step = (xmax-xmin)/float(xbins)
+            xarray = [ xmin+step*xbin for xbin in range(xbins) ]
+            yarray = histobj["yvalues"]
+            data = [dict(x0=xmin, dx=step, y=yarray, type="bar")]
+          fig = dict(data=data, layout=layout, config = {"responsive": True, 'displayModeBar': True} )
           packet[histname] = {'figure': fig, 'time': float(timestamp)}
 
 
                 #print(f"data from lastHistogram:{json.dumps(fig)}")
       yield f"data:{json.dumps(packet)}\n\n"
-      time.sleep(10)
+      time.sleep(30)
   return Response(getHistogram(), mimetype='text/event-stream')
+
+
+
+  # def readJSONTagsfile():
+  #   with open('tags.json') as f:
+  #     data = json.load(f)
+  #   return data
+
+  # def writeJSONTagsfile(data):
+  #   with open('tags.json') as f:
+  #     #celui là qui va comparer nouveaux tags et les anciens et va écrire seulement
+
+    

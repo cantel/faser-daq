d756148d976bda43f8d48f89987d52f3c98f8b60
karlmtr <none>
2020-08-16 14:32:16 +0200

    added tag search

--- scripts/Monitoring/Dashboard/flaskDashboard/__init__.py
+++ scripts/Monitoring/Dashboard/flaskDashboard/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_session import Session
+
 
 app = Flask(__name__)
-SESSION_TYPE = 'filesystem'
+
 app.config.from_object(__name__)
-Session(app)
+
 # app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 from flaskDashboard import routes
--- scripts/Monitoring/Dashboard/flaskDashboard/routes.py
+++ scripts/Monitoring/Dashboard/flaskDashboard/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request, Response, jsonify, session
+from flask import render_template, url_for, flash, redirect, request, Response, jsonify
 from flaskDashboard import app
 import plotly
 import plotly.graph_objs as go
@@ -9,70 +9,122 @@ import redis
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import time
-import requests
+import os 
 
-def getselectedModuleInfoFUNCTION(module):
-  data = r.hgetall(module)
-  keys = []
-  for key in data.keys():
-    if data[key].find('{') == -1:
-      continue
-    else:
-      keys.append(key)
-  packet = {'name': module, 'info': keys}
-  return packet
-
-def getcurrentModulesFUNCTION():
-  database_modules = r.scan() ##returns the number of the database and the modules in it
-  modules = [] 
-# Add only modules with hash data
-  for module in database_modules[1]:
-    try: 
-      r.hgetall(module)
-    except redis.exceptions.ResponseError:
-      print(f"{module} doesn't contain hash data")
-      continue
-    modules.append(module)
-  packet = {"data": modules}
-  return packet
-
-plotly_types = {'num_fixedwidth': "bar", '2d_num_fixedwidth': "histogram2d"}
 r = redis.Redis(host='localhost', port=6379, db=0,charset="utf-8", decode_responses=True)
+r5 = redis.Redis(host='localhost', port=6379, db=5,charset="utf-8", decode_responses=True)
 
 
-@app.route("/", methods=["GET", "POST"])
+#pas fini
+@app.route("/getIDs",methods=["GET"])
+def getIDs():
+  modules= getModules().get_json() 
+  keys = []
+  for module in modules:
+    histnames = r.hkeys(module)
+    for histname in histnames:
+      if "h_" in histname:
+        keys.append(f"{module}-{histname[2:]}")
+  return jsonify(keys)
+
+@app.route("/storeDefaultTagsAndIDs",methods=["GET"] )
+def storeDefaultTagsAndIDs():
+  ids = getIDs().get_json()
+  for ID in ids:
+    module,histname = ID.split("-")
+    r5.sadd(f"tag:{module}",ID)
+    r5.sadd(f"tag:{histname}",ID)
+    r5.sadd(f"id:{ID}", module, histname)
+  return "true"
+
+@app.route("/getAllTags", methods = ["GET"])
+def getAllTags():
+  tags = r5.keys("tag*")
+  tags = [s.replace("tag:","") for s in tags]
+  return jsonify(tags)
+
+@app.route("/getAllIDs")
+def getAllIDs():
+  ids = r5.keys("id:*")
+  ids = [s.replace("id:","") for s in ids]
+  return jsonify(ids)
+
+
+def getTagsByID(ids):
+  packet = {}
+  for ID in ids:
+    module, histname = ID.split("-")
+    packet[ID] = list(r5.smembers(f"id:{ID}"))
+    packet[ID].pop(packet[ID].index(module))
+    packet[ID].pop(packet[ID].index(histname))
+  return packet
+
+@app.route("/getModules", methods= ["GET"])
+def getModules():
+  modules = r.keys("*monitor*")
+  return jsonify(modules)
+
+@app.route("/saveDB")
+def saveDB():
+  # r5.saveBG ## vérifier la commande 
+  return "true"
+
+
+@app.route("/addTag", methods=["GET"])
+def addTag():
+  tag_to_add = request.args.get("tag")
+  ID, tag = tag_to_add.split(",")
+  r5.sadd(f"id:{ID}", tag )
+  r5.sadd(f"tag:{ tag }", ID)
+  return "true"
+
+
+@app.route("/removeTag", methods=["GET"])
+def removeTag():
+  tag_to_remove = request.args.get("tag")
+  ID,tag = tag_to_remove.split(",")
+  r5.srem(f"id:{ID}",tag)
+  r5.srem(f"tag:{tag}",ID)
+  return "true"
+
+@app.route("/renameTag", methods=["GET"])
+def renameTag():
+  tag = request.ars.get("tag")
+  ID,oldname,newname = tag.split(",")
+  r5.srem(f"id:{ID}", oldname)
+  r5.sadd(f"id:{ID}", newname)
+  r5.srem(f"tag:{oldname}",ID)
+  r5.sadd(f"tag:{ newname }", ID)
+
+  return "true"
+
+# pas fini
+@app.route("/update")
+def update():
+  # update modules et tags si besoin?
+  pass
+  #return redirect?
+
+@app.route("/home")
 def home():
-  # if not request.form.get('module_select'):
-  if request.method == 'POST':
-    session["flash_module"] = True
-    selectedModule = request.form.get('module_select')
-    return redirect(url_for('monitormodule', module = selectedModule))
-  else:
-    # if session.get("flash_module")== True:
-    flash("Modules updated", "success")
-    session["flash_module"]= False
-    print("Doing the busy stuff")
-    dataModules = getcurrentModulesFUNCTION()
-    session['modules'] = dataModules
-    return render_template('home.html', modules = dataModules["data"])
+  modules = getModules().get_json()
+  return render_template('home.html')
 
+@app.route('/')
+def load():
+  print("Doing the busy stuff")
+  storeDefaultTagsAndIDs() ## put the basic tags module and histname 
+  # récupérer sauvegarde si elle existe
+  return redirect(url_for('home'))
 
-@app.route('/monitor/<string:module>', methods=["GET", "POST"])
-def monitormodule(module):
-  moduleData = getselectedModuleInfoFUNCTION(module)
-  modules = session.get('modules')
-  return render_template('module.html', modules = modules, selected_module = moduleData)
-
-  
-@app.route("/<string:sourceid>/<string:nameshisto>")
-def lastHistogram(sourceid, nameshisto):
+@app.route("/source/<path:path>")
+def lastHistogram(path):
   def getHistogram():
-    source = sourceid
-    histnames = nameshisto.split("-")
-    # print(histnames)
     while True:
       packet = {}
-      for histname in histnames :
+      ids = path.split("/")
+      for ID in ids:
+        source,histname = ID.split("-")
         histobj=r.hget(source,histname) #lrange: one value: the first in the list.
         if histobj != None:
           timestamp = histobj[:histobj.find(':')]
@@ -113,17 +165,17 @@ def lastHistogram(sourceid, nameshisto):
             ybins = int(histobj["ybins"])
             ystep = (ymax-ymin)/float(ybins)
             yarray = [ ymin+ystep*ybin for ybin in range(ybins) ]
-            print("xbins:", xbins)
-            print("xstep:", xstep)
-            print("ybins:", ybins)
-            print("ystep:", ystep)
-            print("xarray", len(xarray))
-            print("yarray", len(xarray))
-            print("\n \n \n \n \n ")
+            # print("xbins:", xbins)
+            # print("xstep:", xstep)
+            # print("ybins:", ybins)
+            # print("ystep:", ystep)
+            # print("xarray", len(xarray))
+            # print("yarray", len(xarray))
+            # print("\n \n \n ")
             
             # data = [dict(xbins=dict(start=xmin, end=xmax,size=xstep), ybins=dict(start=ymin, end=ymax,size=ystep), z=zarray[106:], type="heatmap")]
             # data = [dict(x0 = xmin, dx = xstep, y0 = ymin, dy = ystep, z = zarray, type="heatmap")]
-            data = [dict(x = xarray, y = yarray, z = zarray, type="heatmap")]
+            data = [dict(x = xarray, y = yarray, z = zarray, type=" heatmap")]
           #  print("xbins = ", xbins)
           #  print("ybins = ", ybins)
           #  print("len(zvalues) = ", len(zarray))
@@ -136,7 +188,7 @@ def lastHistogram(sourceid, nameshisto):
             yarray = histobj["yvalues"]
             data = [dict(x0=xmin, dx=step, y=yarray, type="bar")]
           fig = dict(data=data, layout=layout, config = {"responsive": True, 'displayModeBar': True} )
-          packet[histname] = {'figure': fig, 'time': float(timestamp)}
+          packet[ID] = {'figure': fig, 'time': float(timestamp)}
 
 
                 #print(f"data from lastHistogram:{json.dumps(fig)}")
@@ -144,15 +196,25 @@ def lastHistogram(sourceid, nameshisto):
       time.sleep(30)
   return Response(getHistogram(), mimetype='text/event-stream')
 
+@app.route("/monitor", methods=["GET", "POST"])
+def monitor():
+  if request.method == "GET":
+    if request.args.get("selected_module"):
+      selected_module = request.args.get("selected_module")
+      ids = r5.smembers(f"tag:{selected_module}")
+      modules=getModules().get_json()
+      tagsbyID = getTagsByID(ids)
+      return render_template("monitor.html", selected_module = selected_module, tagsByID = tagsbyID, ids = ids)
+    elif request.args.getlist("selected_tags"):
+      selected_tags = request.args.getlist("selected_tags")
+      selected_tags = [ f"tag:{s}" for s in selected_tags]
+      ids = r5.sunion(selected_tags)
+      tagsbyID = getTagsByID(ids)
+      return render_template("monitor.html", tagsByID = tagsbyID, ids = ids)
 
 
-  # def readJSONTagsfile():
-  #   with open('tags.json') as f:
-  #     data = json.load(f)
-  #   return data
-
-  # def writeJSONTagsfile(data):
-  #   with open('tags.json') as f:
-  #     #celui là qui va comparer nouveaux tags et les anciens et va écrire seulement
-
-    
+@app.context_processor
+def context_processor():
+  modules=getModules().get_json()
+  tags = getAllTags().get_json()
+  return dict(modules = modules, tags = tags)

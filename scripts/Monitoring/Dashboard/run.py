51980f5ae43816b01a28d1adf1711676e18ca1df
karlmtr <none>
2020-09-15 19:33:12 +0200

    added: download tags to json and upload it later, single_view (show only one live graph), historic_view (view old histograms)

--- scripts/Monitoring/Dashboard/run.py
+++ scripts/Monitoring/Dashboard/run.py
@@ -3,6 +3,5 @@ import platform
 
 PORT = 8050
 if __name__ == "__main__":
-    print("Connect to the browser to")
-    print(f"http://{platform.node()}:{PORT}")
-    app.run(debug=True, host="0.0.0.0", port=PORT)
+    print(f"Connect with the browser to http://{platform.node()}:{PORT}")
+    app.run(debug=True, host="0.0.0.0", port=PORT, use_reloader=False)

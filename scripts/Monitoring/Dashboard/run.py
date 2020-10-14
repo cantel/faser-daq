fd4c01ff6b4e757b7b543152d5df64213b115c0b
karlmtr <none>
2020-10-15 00:38:37 +0200

    splitted new experiement option into delete tags and delete current histograms/colored those option in red/removed console.log lines

--- scripts/Monitoring/Dashboard/run.py
+++ scripts/Monitoring/Dashboard/run.py
@@ -4,4 +4,4 @@ import platform
 PORT = 8050
 if __name__ == "__main__":
     print(f"Connect with the browser to http://{platform.node()}:{PORT}")
-    app.run(debug=True, host="0.0.0.0", port=PORT, use_reloader=False)
+    app.run( host="0.0.0.0", port=PORT, debug=True, use_reloader =False )

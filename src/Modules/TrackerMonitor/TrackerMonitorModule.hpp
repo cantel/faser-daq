f46aeeb1a70bf8f891902545e4c106a2531a0db5
Di Wang <di.wang@cern.ch>
2020-11-25 08:53:33 +0100

    Add 2d histogram of chip_occupancy

--- src/Modules/TrackerMonitor/TrackerMonitorModule.hpp
+++ src/Modules/TrackerMonitor/TrackerMonitorModule.hpp
@@ -20,5 +20,5 @@ class TrackerMonitorModule : public MonitorBaseModule {
   const uint8_t kSTRIPDIFFTOLERANCE = 25; // FIXME can be tuned
   const uint8_t kSTRIPS_PER_CHIP = 128;
   const uint8_t kCHIPS_PER_MODULE = 12;
-
+  const uint8_t kTOTAL_MODULES = 8;
 };

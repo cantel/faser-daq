8247ecf1ece3efbb628212024525e9e30d44f29e
aagaard <Brian.Petersen@cern.ch>
2020-12-02 19:56:08 +0100

    Prototype run info service

---
+++ scripts/RunService/examples/example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Get run number"
+runno=`curl -s -u FASER:HelloThere -d '{"type":"calibration", "version":"v1.2.0", "configuration": {"a":"b","c":[1,2,3]}, "valid":false}' -H "Content-Type: application/json" -X POST http://faser-daq-001:5002/NewRunNumber`
+echo "Got: $runno"
+echo "End run and set some run data"
+curl -u FASER:HelloThere -d '{"runinfo": {"d": [3,45]}}' -H "Content-Type: application/json" -X POST http://faser-daq-001:5002/AddRunInfo/$runno
+echo "Get run information"
+curl http://faser-daq-001:5002/RunInfo/$runno
---
+++ scripts/RunService/examples/runnumber.cxx
@@ -0,0 +1,48 @@
+#include <cpr/cpr.h>
+#include <iostream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+int main(int argc, char** argv) {
+
+  json newRun=R"(
+                { 
+                  "type": "noisecalibration",
+                  "configName": "threestation",
+		  "version": "v1.2.0",
+		  "configuration": 
+                    { "a": "b",
+                      "c": [1,2,3]
+		    }
+                })"_json;
+  
+  cpr::Response r = cpr::Post(cpr::Url{"http://faser-daq-001:5002/NewRunNumber"},
+			      cpr::Authentication{"FASER", "HelloThere"},
+			      cpr::Body{newRun.dump()},
+			      cpr::Header{{"Content-Type", "application/json"}});
+  if (r.status_code != 201) {
+    std::cerr << "Error [" << r.status_code << "] making request" << std::endl;
+    return 1;
+  }
+  std::cout << "New run number: " << std::endl<<r.text << std::endl;
+  int runno=std::stoi(r.text);
+
+  json endRun=R"( { "runinfo": { "numEvents": 1000 } } )"_json;
+  r = cpr::Post(cpr::Url{"http://faser-daq-001:5002/AddRunInfo/"+std::to_string(runno)},
+		cpr::Authentication{"FASER", "HelloThere"},
+		cpr::Body{endRun.dump()},
+		cpr::Header{{"Content-Type", "application/json"}});
+  if (r.status_code != 200) {
+    std::cerr << "Error [" << r.status_code << "] making request" << std::endl;
+    return 1;
+  }
+  std::cout << "UpdateStatus: "<<r.text << std::endl;
+
+  r = cpr::Get(cpr::Url{"http://faser-daq-001:5002/RunInfo/"+std::to_string(runno)});
+  if (r.status_code != 200) {
+    std::cerr << "Error [" << r.status_code << "] making request" << std::endl;
+    return 1;
+  }
+  std::cout << "Run information:"<<std::endl<<r.text << std::endl;
+}

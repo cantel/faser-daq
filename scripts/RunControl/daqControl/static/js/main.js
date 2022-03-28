75dd059824d170020aca6d72006cbcaefff95086
Edward Karl Galantay <edward.karl.galantay@cern.ch>
2022-03-29 00:52:20 +0200

    initial commit

---
+++ scripts/RunControl/daqControl/static/js/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+        drawer: false,
+        isDrawerLocked : false,
+    },
+    delimiters: ["[[", "]]"],
+    methods: {
+    },
+});

a47e9a3688630e577a269dda20a4ec1ef2685187
Edward Karl Galantay <edward.karl.galantay@cern.ch>
2022-05-22 23:46:29 +0200

    if number of modules in a category is one, delete the category/ alphabetically ordered/automatic creation of directory

--- scripts/RunControl/createControlTreeConfig.py
+++ scripts/RunControl/createControlTreeConfig.py
@@ -1,7 +1,6 @@
 from copy import deepcopy
 import os,sys
 from pathlib import Path
-from os import environ as env
 import jsonref
 
 lt = {
@@ -20,27 +19,19 @@ lt = {
     "EmulatorMonitor": "EM"
 }
 
-# configPath = sys.argv[1]
-# configPath = "demo-tree/config.json" # path to config file
-
-
-def createJSON(configPath):
-    configDirPath = os.path.join(env['DAQ_CONFIG_DIR'])
+def createTreeJSON(configPath):
     try :
         with open(os.path.join(configDirPath, configPath), "r") as fp :
             base_dir_uri = f"{Path(configDirPath).as_uri()}/"
             jsonref_obj = jsonref.load(fp,base_uri=base_dir_uri,loader=jsonref.JsonLoader())
     except FileNotFoundError:
-        print("Error")
+        print("Error, the file doesn't exist")
         exit(1)
-
-    if "configuration" in jsonref_obj: # faser ones have "configuration" but not demo-tree
-        # schema with references (version >= 10)
+    if "configuration" in jsonref_obj: # faser ones have "configuration" but not demo-tree schema with references (version >= 10) 
         configuration = deepcopy(jsonref_obj)["configuration"]
     else:
         # old-style schema (version < 10)
         configuration = jsonref_obj
-
     tree = {"name" : "Root", "children" :[]}
     sort = {}
     for component in configuration["components"]:
@@ -49,37 +40,55 @@ def createJSON(configPath):
         else:
             sort[component["modules"][0]["type"]] = []
             sort[component["modules"][0]["type"]].append(component["modules"][0]["name"])
-
-    for key,item in sort.items():
+    
+    
+    for key,item in sorted(sort.items()): 
+        # if there is only one module in the categorie
+        if len(item) == 1:
+            cat = {"name" : item[0], "types" : [{"type" : lt[key]}]}  
+            tree["children"].append(cat)
+            continue
+            
         cat = {"name" : key, "types" :[{"type": lt[key]}], "children" : []}
         for comp in item :
             cat["children"].append({"name":comp})
         tree["children"].append(cat)
-
-
     return tree
 
 
+def create_config_dict(configName):
+    # TODO check if file exists 
+
+    linkDict = {
+        "tree": "control-tree.json",
+        "fsm_rules": "../fsm-rules.json",
+        "config": f"../{configName}",
+        "grafana": "../grafana.json"
+    }
+    return linkDict
+    
+
 
 if __name__ == "__main__":
-    
-    if len(sys.argv) != 3 :
-        print(f"USAGE : python {sys.argv[0]} <configPath> <destinationPath>")
+    # configDirPath = os.path.join("/Users/edward/Documents/PROG/FASER/faser-daq/configs")
+    configDirPath = os.path.join(os.environ['DAQ_CONFIG_DIR'])
+
+    if len(sys.argv) != 2 :
+        print(f"USAGE : python {sys.argv[0]}")
         exit(1)
     
-    configPath = sys.argv[1]
-    savePath = sys.argv[2] 
+    configName= sys.argv[1]
 
-    treeConfig = createJSON(configPath)
-     
-    with open(savePath, "w") as fp:
-        jsonref.dump(treeConfig,fp, indent=4)
+    tree_config = createTreeJSON(configName)
+    dict_config = create_config_dict(configName)
 
+    dirPath = os.path.join(configDirPath, configName.replace(".json",""))
+    os.mkdir(dirPath)
 
+    with open(os.path.join(dirPath,"control-tree.json"), "w") as fp:
+        jsonref.dump(tree_config,fp, indent=4)
 
-
-    
-    
-
+    with open(os.path.join(dirPath,"config-dict.json"), "w") as fp:
+        jsonref.dump(dict_config,fp, indent=4)
 
 

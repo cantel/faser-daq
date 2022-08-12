7df1b0406f47a7eb1b2f44962af81dc82bf1915d
Edward Karl Galantay <edward.karl.galantay@cern.ch>
2022-08-12 17:57:33 +0200

    Updated createControlTreeConfig script: will not overwrite existing configs

--- scripts/RunControl/createControlTreeConfig.py
+++ scripts/RunControl/createControlTreeConfig.py
@@ -1,36 +1,37 @@
 from copy import deepcopy
 import os,sys
 from pathlib import Path
-import jsonref
+import jsonref, json
 
 lt = {
-    "ReadoutInterface" : "RI",
-    "EventBuilder" : "EB",
-    "FileWriter" : "FW", 
-
-    "EventBuilderFaser": "EBF", #
-    "FileWriterFaser" : "FWF", #
-
-    "TriggerReceiver" :"TR", # hardware
-    "TriggerGenerator" : "TG", # emulation
-    "TriggerMonitor": "TM",
-    "FrontEndReceiver" : "FR", # hardware
-    "FrontEndMonitor" : "FM",
-    "FrontEndEmulator" : "FE",
-    "EmulatorMonitor": "EM",
+    "ReadoutInterface"  : "RI",
+    "EventBuilder"      : "EB",
+    "FileWriter"        : "FW", 
+    "EventBuilderFaser" : "EBF", #
+    "FileWriterFaser"   : "FWF", #
+    "TriggerReceiver"   : "TR", # hardware
+    "TriggerGenerator"  : "TG", # emulation
+    "TriggerMonitor"    : "TM",
+    "FrontEndReceiver"  : "FR", # hardware
+    "FrontEndMonitor"   : "FM",
+    "FrontEndEmulator"  : "FE",
+    "EmulatorMonitor"   : "EM",
     "DigitizerReceiver" : "DR",
-    "SCTDataMonitor":"SDM",
-    "TrackerReceiver": "TKR",
+    "SCTDataMonitor"    : "SDM",
+    "TrackerReceiver"   : "TKR",
     "TriggerRateMonitor": "TRM",
+    "DigitizerMonitor"  : "DM",
+    "EventPlayback"     : "EP",
+    "TrackStationMonitor":"TSM"
 }
 
-def createTreeJSON(configPath):
+def createTreeJSON(configPath,configsDirPath):
     try :
-        with open(os.path.join(configDirPath, configPath), "r") as fp :
-            base_dir_uri = f"{Path(configDirPath).as_uri()}/"
+        with open(configPath, "r") as fp :
+            base_dir_uri = f"{Path(configsDirPath).as_uri()}/"
             jsonref_obj = jsonref.load(fp,base_uri=base_dir_uri,loader=jsonref.JsonLoader())
     except FileNotFoundError:
-        print("Error, the file doesn't exist")
+        print("Error, the file does not exist")
         exit(1)
     if "configuration" in jsonref_obj: # faser ones have "configuration" but not demo-tree schema with references (version >= 10) 
         configuration = deepcopy(jsonref_obj)["configuration"]
@@ -48,52 +49,68 @@ def createTreeJSON(configPath):
     
 
     for key,item in sorted(sort.items()): 
-        # if there is only one module in the categorie
-        if len(item) == 1:
-            cat = {"name" : item[0], "types" : [{"type" : lt[key]}]}  
-            tree["children"].append(cat)
-            continue
-            
-        cat = {"name" : key, "types" :[{"type": lt[key]}], "children" : []}
+        # if there is only one module in the categorie, there is no category
+        try : 
+            if len(item) == 1:
+                cat = {"name" : item[0], "types" : [{"type" : lt[key]}]}  
+                tree["children"].append(cat)
+                continue
+            cat = {"name" : key, "types" :[{"type": lt[key]}], "children" : []}
+        except KeyError :
+            print(f"Module type '{key}' is not in the lookup table for abbrevations, please add it.")
+            exit(1)
+
         for comp in item :
             cat["children"].append({"name":comp})
         tree["children"].append(cat)
     return tree
 
 
-def create_config_dict(configName):
-    # TODO check if file exists 
-
-    linkDict = {
-        "tree": "control-tree.json",
-        "fsm_rules": "../fsm-rules.json",
-        "config": f"../{configName}",
-        "grafana": "../grafana.json"
-    }
-    return linkDict
-    
-
-
 if __name__ == "__main__":
-    # configDirPath = os.path.join("/Users/edward/Documents/PROG/FASER/faser-daq/configs")
-    configDirPath = os.path.join(os.environ['DAQ_CONFIG_DIR'])
 
+    
     if len(sys.argv) != 2 :
-        print(f"USAGE : python {sys.argv[0]} <configName>")
+        print(f"USAGE : python {sys.argv[0]} <configFullPath>")
         exit(1)
     
-    configName= sys.argv[1]
+    configPath     = sys.argv[1]                                  # full path to config
+    configsDirPath = os.path.dirname(os.path.abspath(configPath)) # full path to containing dir
+    configName     = os.path.basename(configPath)                 # name of the config with extension
+    configDir      = os.path.join(configsDirPath,configName.replace('.json','')) # dir of specific config
+    dict_obj = {
+        "tree"     : "control-tree.json",
+        "fsm_rules": "../fsm-rules.json",
+        "config"   : f"../{configName}",
+        "grafana"  : "../grafana.json"
+    }
+    with open(os.path.join(configsDirPath,"fsm-rules.json"), "r") as fp:
+        fsm_rules = json.load(fp)
 
-    tree_config = createTreeJSON(configName)
-    dict_config = create_config_dict(configName)
 
-    dirPath = os.path.join(configDirPath, configName.replace(".json",""))
-    os.mkdir(dirPath)
+    tree_config = createTreeJSON(configPath, configsDirPath)
 
-    with open(os.path.join(dirPath,"control-tree.json"), "w") as fp:
-        jsonref.dump(tree_config,fp, indent=4)
+    try : 
+        os.mkdir(configDir)
+        with open(os.path.join(configDir,"control-tree.json"), "w") as fp:
+            jsonref.dump(tree_config,fp, indent=4)
+        with open(os.path.join(configDir,"config-dict.json"), "w") as fp:
+            jsonref.dump(dict_obj,fp, indent=4)
 
-    with open(os.path.join(dirPath,"config-dict.json"), "w") as fp:
-        jsonref.dump(dict_config,fp, indent=4)
+    except FileExistsError:
+        print("Directory already exists ! ")
+        r = input("Do you want to overwrite it ? (y/N) ")
+        if (r.upper() == "Y"):
+            with open(os.path.join(configDir,"control-tree.json"), "w") as fp:
+                jsonref.dump(tree_config,fp, indent=4)
+            with open(os.path.join(configDir,"config-dict.json"), "w") as fp:
+                jsonref.dump(dict_obj,fp, indent=4)
+        else:
+            print("Exiting...")
+            exit(0)
+    
+    # checking if all the types are in the fsm-rules order > start/stop arrays 
+
+
+
 
 

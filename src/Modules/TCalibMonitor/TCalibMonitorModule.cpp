81d3f4dca69067afd97ff7224556cc0e646d2537
Savannah Rose Shively <sshively@faser-daqvm-000.cern.ch>
2022-09-14 10:11:33 +0200

    functional!

--- src/Modules/TCalibMonitor/TCalibMonitorModule.cpp
+++ src/Modules/TCalibMonitor/TCalibMonitorModule.cpp
@@ -8,15 +8,37 @@
 #include <iostream> // std::flush
 #include <sstream> // std::ostringstream
 #include <fstream>      // std::ofstream
+#include "Utils/Ers.hpp"
 /// \endcond
 
 
 #include "TCalibMonitorModule.hpp"
-#include "Utils/Ers.hpp"
+//using //namespace chrono::chrono_literals;
+//using //namespace chrono::chrono;
 
-TCalibMonitorModule::TCalibMonitorModule(const std::string& n):FaserProcess(n) { ERS_INFO(""); }
 
-TCalibMonitorModule::~TCalibMonitorModule() { ERS_INFO(""); }
+
+TCalibMonitorModule::TCalibMonitorModule(const std::string& n): MonitorBaseModule(n),m_prefix_hname_hitp("hitpattern_physics_mod"),m_hname_scterrors("sct_data_errors"){
+   INFO("TCalibMonitorModule()");
+   auto cfg = getModuleSettings();
+   auto cfg_trigbits_select = cfg["PhysicsTriggerBits"];
+   if (cfg_trigbits_select!="" && cfg_trigbits_select!=nullptr)
+      m_physics_trigbits = cfg_trigbits_select;
+   else m_physics_trigbits=0xf;
+
+}
+
+TCalibMonitorModule::~TCalibMonitorModule() { 
+  INFO("With config: " << m_config.dump());
+}
+
+void TCalibMonitorModule::start(unsigned int run_num){
+  INFO("Starting "<<getName());
+  //m_hit_avg = 0;
+  //m_hit_avg_count = 0;
+  MonitorBaseModule::start(run_num);
+}
+
 
 // optional (configuration can be handled in the constructor)
 void TCalibMonitorModule::configure() {
@@ -27,54 +49,162 @@ void TCalibMonitorModule::configure() {
   //name histo accordingly
 }
 
-void TCalibMonitorModule::start(unsigned run_num) {
-  FaserProcess::start(run_num);
-  ERS_INFO("");
-}
 
-void TCalibMonitorModule::stop() {
-  FaserProcess::stop();
-  ERS_INFO("");
-}
+void TCalibMonitorModule::monitor(DataFragment<daqling::utilities::Binary> &eventBuilderBinary) {
 
-void TCalibMonitorModule::runner() noexcept {
-  ERS_INFO("Running...");
-  while (m_run) {
-    //m_histogrammanager->fill("good_nhits_physics",goodHits) //one for high, one for low, x = strip id's
-    //histogram per module; 12 chips x 127 channels = 1000-ish
-    //higher and lower threshold bin for sure - could do instead: want hits per strip for each of the chips, per module.
-    //    a histogram per module. then
-    // on x = strip id. y axis: chip id 0-11. OR histo per chip. 8x12 histos for mask scans
-    // display and analysis histos. display is low granulatrity
+  auto evtHeaderUnpackStatus = unpack_event_header(eventBuilderBinary);
+  if (evtHeaderUnpackStatus) return;
+
+  if ( m_event->event_tag() != m_eventTag ) {
+    ERROR("Event tag does not match filter tag. Are the module's filter settings correct?");
+    return;
   }
-  ERS_INFO("Runner stopped");
+
+  bool randTrig(false);
+  bool physicsTrig(false);
+  auto fragmentUnpackStatus = unpack_full_fragment( eventBuilderBinary, SourceIDs::TriggerSourceID );
+  if ( fragmentUnpackStatus ) {
+     WARNING("Unpacking error for trigger fragment. Can't use trigger information for this event!");
+  } else {
+    if (m_tlbdataFragment->tbp() & 0x10) randTrig = true;
+    if ((m_tlbdataFragment->tbp()&0xf) & m_physics_trigbits) physicsTrig=true;
+  }
+
+
+  if ( m_trackerdataFragment ){
+    if (m_trackerdataFragment->has_trb_error()) m_histogrammanager->fill("track_data_error_types", "TRBError");
+    if (m_trackerdataFragment->has_module_error()) m_histogrammanager->fill("track_data_error_types", "ModuleError");
+    if (m_trackerdataFragment->missing_event_id()) m_histogrammanager->fill("track_data_error_types", "NoEventID");
+    if (m_trackerdataFragment->missing_bcid()) m_histogrammanager->fill("track_data_error_types", "NoBCID");
+    if (m_trackerdataFragment->missing_crc()) m_histogrammanager->fill("track_data_error_types", "NoCRC");
+    if (m_trackerdataFragment->missing_frames()) m_histogrammanager->fill("track_data_error_types", "MissingFrames");
+    if (m_trackerdataFragment->unrecognized_frames()) m_histogrammanager->fill("track_data_error_types", "UnrecognizedFrames");
+    if (!m_trackerdataFragment->valid()) {
+      WARNING("Errors were found in tracker fragment:\n"<<*m_trackerdataFragment<<"\n Skipping event!");
+      m_metric_total_errors++;
+      return;
+    }
+  }
+  else {WARNING("Ignoring empty data fragment.");return;}
+
+  size_t payload_size = m_fragment->payload_size();
+  if ( payload_size > MAXFRAGSIZE ) {
+     WARNING(" VERY large payload size received. Payload size of "<<payload_size<<" bytes exceeds maximum allowable for histogram filling. Resetting to "<<MAXFRAGSIZE);
+     payload_size = MAXFRAGSIZE;
+  } 
+  m_histogrammanager->fill("payloadsize", payload_size);
+
+  m_bcid = m_trackerdataFragment->bc_id();
+  m_l1id = m_trackerdataFragment->event_id();
+  m_histogrammanager->fill("bcid", m_bcid);
+
+  for ( auto it = m_trackerdataFragment->cbegin(); it != m_trackerdataFragment->cend(); ++it ){
+      auto sctEvent = *it;
+      if (sctEvent == nullptr) { 
+        WARNING("Invalid SCT Event for event "<<m_trackerdataFragment->event_id());
+        WARNING("tracker data fragment: "<<*m_trackerdataFragment);
+        m_status = STATUS_WARN;
+        m_histogrammanager->fill("track_data_error_types", "ModuleDecodeError");
+        continue;
+      }
+
+      // how about SCT data errors
+     std::string hname_mod_scterrors = m_hname_scterrors+"_mod"+std::to_string(sctEvent->GetModuleID());
+     if ( sctEvent->HasError() ) {
+        auto sctErrorList= sctEvent->GetErrors();
+        for ( unsigned idx = 0; idx < 12; idx++ ) {
+          auto sctErrors = sctErrorList.at(idx);
+          for ( uint8_t sctError : sctErrors ) {
+            WARNING("SCT Error for module "<<sctEvent->GetModuleID()<<", chip idx "<<idx<<". Error code = "<<static_cast<int>(sctError));
+            //m_total_WARNINGS++;
+            switch ( sctError ) {
+              case 0x1:
+               m_histogrammanager->fill(hname_mod_scterrors, "NoData");
+               m_histogrammanager->fill(m_hname_scterrors, "NoData");
+               break;
+              case 0x2:
+               m_histogrammanager->fill(hname_mod_scterrors, "BuffOverflow");
+               m_histogrammanager->fill(m_hname_scterrors, "BuffOverflow");
+               break;
+              case 0x4:
+               m_histogrammanager->fill(hname_mod_scterrors, "BuffError");
+               m_histogrammanager->fill(m_hname_scterrors, "BuffError");
+               break;
+              case 0xff:
+               m_histogrammanager->fill(hname_mod_scterrors, "UnknownChip");
+               m_histogrammanager->fill(m_hname_scterrors, "UnknownChip");
+               break;
+              default:
+               m_histogrammanager->fill(hname_mod_scterrors, "Unknown");
+               m_histogrammanager->fill(m_hname_scterrors, "Unknown");
+            }
+          }
+        }
+      }; // end of SCT ERRORs check
+
+
+
+      uint8_t module = sctEvent->GetModuleID();
+      auto allHits = sctEvent->GetHits();
+
+      if (physicsTrig && m_lhc_physics_mode) { // only selected physics triggered events during LHC collisions
+        //std::string hname_hitp = m_prefix_hname_hitp+std::to_string(sctEvent->GetModuleID());
+        for ( unsigned chipIdx = 0; chipIdx < (unsigned)kCHIPS_PER_MODULE*0.5; chipIdx++) {
+          auto hitsPerChip1 = allHits[chipIdx];
+          for (auto hit1 : hitsPerChip1){ // hit is an std::pair<uint8 strip, uint8 pattern>
+            if ( hit1.second == 7 ) continue;
+            unsigned chipIdx2 = kCHIPS_PER_MODULE - 1 - chipIdx;
+            m_histogrammanager->fill2D("hitmap_physics", module, chipIdx, 1);
+            m_histogrammanager->fill2D("hitmap_physics", module, chipIdx2, 1);
+          }
+        }
+
+      }
+
+
+  //uint8_t module = sctEvent->GetModuleID();
+    if (randTrig) {
+          for ( unsigned chipIdx = 0; chipIdx < 12; chipIdx++) {
+            auto hitsPerChip = allHits[chipIdx];
+            for (auto hit : hitsPerChip){
+                std::bitset<3> bitset_hitp(hit.second);
+                m_histogrammanager->fill("hitpattern_random", bitset_hitp.to_string());
+                if ( hit.second == 7 || hit.second == 0 ) continue;
+                  m_histogrammanager->fill2D("hitmap_random", module, chipIdx, 1);
+            
+            }
+          }
+        }
+  }
 }
 
-//--------------------------
+void TCalibMonitorModule::register_hists() {
 
+  INFO(" ... registering histograms in SCTDataMonitor ... " );
 
-//using namespace daqling::core;
-//using namespace daqling::module;
-//HistFiller::HistFiller(const std::string &n) : DAQProcess(n) { ERS_INFO(""); }
+  m_histogrammanager->registerHistogram("payloadsize", "payload size [bytes]", 0, MAXFRAGSIZE/50, MAXFRAGSIZE/2000,  Axis::Range::EXTENDABLE, m_PUBINT*10);
+
+  m_histogrammanager->registerHistogram("bcid", "BCID", -0.5, 3564.5, 3565, 1800);
+  m_histogrammanager->register2DHistogram("hitmap_physics", "module idx", 0, kTOTAL_MODULES, kTOTAL_MODULES, "chip idx",  0, kCHIPS_PER_MODULE, kCHIPS_PER_MODULE, m_PUBINT);
+  m_histogrammanager->register2DHistogram("hitmap_random", "module idx", 0, kTOTAL_MODULES, kTOTAL_MODULES, "chip idx",  0, kCHIPS_PER_MODULE, kCHIPS_PER_MODULE, m_PUBINT);
+
+  std::vector<std::string> trb_error_categories = {"TRBError", "ModuleError", "NoEventID", "NoBCID", "NoCRC", "MissingFrames", "UnrecognizedFrames", "ModuleDecodeError"};
+  m_histogrammanager->registerHistogram("track_data_error_types", "error type", trb_error_categories, m_PUBINT);
+
+  std::vector<std::string> sct_error_categories = {"L1IDMismatch", "BCIDMismatch", "NoData", "BuffOverflow", "BuffError", "UnknownChip", "Unknown"};
+  for ( unsigned i = 0; i < kTOTAL_MODULES; i++ ){
+    std::string hname_mod_scterr = m_hname_scterrors+"_mod"+std::to_string(i);
+    m_histogrammanager->registerHistogram(hname_mod_scterr, "error type", sct_error_categories, m_PUBINT);
+  }
+  m_histogrammanager->registerHistogram(m_hname_scterrors, "error type", sct_error_categories, m_PUBINT); 
+
+  INFO(" ... done registering histograms ... " );
+
+  return ;
+
+}
+
 /*
-
-void HistFiller::start(unsigned run_num) {
-  daqling::core::DAQProcess::start(run_num);
-  ERS_INFO("");
-}
-
-void HistFiller::stop() {
-  daqling::core::DAQProcess::stop();
-  ERS_INFO("");
-}
-
-void HistFiller::runner() noexcept {
-  ERS_INFO("Running...");
-  while (m_run) {
-  }
-  ERS_INFO("Runner stopped");
-}
-
 
 void TCalibMonitorModule::foobar(const std::string &arg) {
   ERS_INFO("Inside custom command. Got argument: " << arg);
